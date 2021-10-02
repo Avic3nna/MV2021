93ac7938c57ee4a422ebacc9b35f1fb996d8fb4e
JavierGalindos <javiergalindos@gmail.com>
2021-10-02 19:56:51 +0300

    Add template_matching function & add figures code

--- Homework 2/Matlab/pattern_match.m
+++ Homework 2/Matlab/pattern_match.m
@@ -10,10 +10,10 @@ camHeight = 480;
 camWidth = 640;
 %% Import template images
 
-template_orange = imread('Images\orange_temp.png');
-template_yellow = imread('Images\yellow_temp.png');
-template_blue = imread('Images\blue_template.png');
-template_empty = imread('Images\empty-temp.png');
+template_orange = imread('Images/orange_temp.png');
+template_yellow = imread('Images/yellow_temp.png');
+template_blue = imread('Images/blue_template.png');
+template_empty = imread('Images/empty-temp.png');
 
 templates ={template_orange; template_yellow; template_blue; template_empty};
 colours = [[0,165,255];
@@ -21,18 +21,12 @@ colours = [[0,165,255];
            [255,0,0];
            [0,0,0]];
 
-img = imread('Images\checker_board6.png');
+img = imread('Images/checker_board6.png');
 
 
 %% Dirk-jan his function template_matching
 template = template_empty;
 [I_SSD,I_NCC, Idata] = template_matching(template, img);
-
- % Find maximum correspondence
-[x,y,z]=ind2sub(size(I_SSD),find(I_SSD==max(I_SSD(:))));
-% disp(x);
-% disp(y);
-% disp(z);
 
 corr_prob = 0.8;
 
@@ -49,26 +43,21 @@ save_means = [];
 mean_cluster = [];
 for i=1:length(unique(clusters))
     mean_cluster(i,:) = mean(cluster_data(clusters == i,:));
-    %mean_cluster(i,2) = mean(cluster_data(clusters == i));
-    
 end
 
 save_means = [save_means; mean_cluster, 1*ones(size(mean_cluster,1),1)];
 origin = mean_cluster(1,:);
 
 figure, 
-imshow(img); hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'k*'); title('Result')
+imshow(img); hold on;
+plot(mean_cluster(:,1),mean_cluster(:,2),'k*');
+text(mean_cluster(:,1) - 20,mean_cluster(:,2) - 20,'empty')
+title('Result')
 
 %% Dirk-jan his function template_matching
 template = template_blue;
 [I_SSD,I_NCC, Idata] = template_matching(template, img);
 
- % Find maximum correspondence
-[x,y,z]=ind2sub(size(I_SSD),find(I_SSD==max(I_SSD(:))));
-% disp(x);
-% disp(y);
-% disp(z);
-
 corr_prob = 0.8;
 
 [B,I] = maxk(I_NCC(:), length(find(I_NCC>corr_prob)));
@@ -84,22 +73,15 @@ clusters = dbscan(cluster_data, pill_radius, min_pts);
 mean_cluster = [];
 for i=1:length(unique(clusters))
     mean_cluster(i,:) = mean(cluster_data(clusters == i,:));
-    %mean_cluster(i,2) = mean(cluster_data(clusters == i));
-    
 end
 save_means = [save_means; mean_cluster, 2*ones(size(mean_cluster,1),1)];
-%save_means = [save_means 2*ones(size(mean_cluster,1))]
-hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'r*'); title('Result')
+hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'r*');
+text(mean_cluster(:,1) - 15,mean_cluster(:,2) - 20,'blue')
+title('Result')
 %% Dirk-jan his function template_matching
 template = template_orange;
 [I_SSD,I_NCC, Idata] = template_matching(template, img);
 
- % Find maximum correspondence
-[x,y,z]=ind2sub(size(I_SSD),find(I_SSD==max(I_SSD(:))));
-% disp(x);
-% disp(y);
-% disp(z);
-
 corr_prob = 0.8;
 
 [B,I] = maxk(I_NCC(:), length(find(I_NCC>corr_prob)));
@@ -115,22 +97,16 @@ clusters = dbscan(cluster_data, pill_radius, min_pts);
 mean_cluster = [];
 for i=1:length(unique(clusters))
     mean_cluster(i,:) = mean(cluster_data(clusters == i,:));
-    %mean_cluster(i,2) = mean(cluster_data(clusters == i));
-    
 end
 save_means = [save_means; mean_cluster, 3*ones(size(mean_cluster,1),1)];
-hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'g*'); title('Result')
+hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'g*'); 
+text(mean_cluster(:,1) - 25,mean_cluster(:,2) - 20,'orange')
+title('Result')
 
 %% Dirk-jan his function template_matching
 template = template_yellow;
 [I_SSD,I_NCC, Idata] = template_matching(template, img);
 
- % Find maximum correspondence
-[x,y,z]=ind2sub(size(I_SSD),find(I_SSD==max(I_SSD(:))));
-% disp(x);
-% disp(y);
-% disp(z);
-
 corr_prob = 0.8;
 
 [B,I] = maxk(I_NCC(:), length(find(I_NCC>corr_prob)));
@@ -146,11 +122,11 @@ clusters = dbscan(cluster_data, pill_radius, min_pts);
 mean_cluster = [];
 for i=1:length(unique(clusters))
     mean_cluster(i,:) = mean(cluster_data(clusters == i,:));
-    %mean_cluster(i,2) = mean(cluster_data(clusters == i));
-    
 end
 save_means = [save_means; mean_cluster, 4*ones(size(mean_cluster,1),1)];
-hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'b*'); title('Result')
+hold on; plot(mean_cluster(:,1),mean_cluster(:,2),'b*');
+text(mean_cluster(:,1) - 20,mean_cluster(:,2) - 20,'yellow')
+title('Result')
 
 %% Compute manhattan distance
 manhattan_dist_pix = pdist2(origin,save_means(:,1:2),'cityblock'); %manhattan distance
@@ -166,3 +142,5 @@ manhattan_dist_mm = [manhattan_dist_mm' save_means(:,3)]
 % 2 is blue
 % 3 is orange
 % 4 is yellow
+
+text(save_means(:,1) - 7,save_means(:,2) + 20,string(round(manhattan_dist_mm(:,1))))
